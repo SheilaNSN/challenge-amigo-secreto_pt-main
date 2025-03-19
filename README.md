5f7176ebf8d1c56fe16a63b9fbbdf65143efec4b
Sheila Nery <sheila.sane@gmail.com>
2025-03-18 23:37:37 -0300

    Add a funcionalidade do projeto

---
+++ README.md
@@ -0,0 +1,21 @@
+```
+
+# Portfólio Olá, Bem vindo ao meu projeto de portfólio.
+
+![image](src="assets\amigo-secreto.png")
+
+## Ferramentas utilizadas:
+
+* HTML
+
+* CSS
+
+* Flex-box
+
+## Feito por:
+
+### Sheila Nery
+
+### Linkedin: https://www.linkedin.com/sheila-nery/
+
+```
--- app.js
+++ app.js
@@ -1,1 +1,70 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  const novoAmigo = document.getElementById("amigo");
+  const nome = novoAmigo.value.trim();
+
+  if (nome === "") {
+    alert("Por favor, digite um nome válido.");
+    return;
+  }
+
+  if (amigos.includes(nome)) {
+    alert("Este nome já foi adicionado.");
+    return;
+  }
+
+  amigos.push(nome);
+  novoAmigo.value = "";
+  atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+  const listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  amigos.forEach((amigo, index) => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+
+    // Botão para excluir o amigo selecionado
+    const botaoRemover = document.createElement("button");
+    botaoRemover.innerHTML = '<i class="fa fa-trash"></i>'; // Ícone da lixeira do Font Awesome
+    botaoRemover.classList.add("button-remove");
+    botaoRemover.onclick = () => removerAmigo(index);
+
+    li.appendChild(botaoRemover);
+    listaAmigos.appendChild(li);
+  });
+}
+
+function removerAmigo(index) {
+  amigos.splice(index, 1); // Remove da lista
+  atualizarListaAmigos();
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Adicione pelo menos um amigo para sortear.");
+    return;
+  }
+
+  const listaResultado = document.getElementById("resultado");
+  listaResultado.innerHTML = "";
+
+  const amigoSorteado = sortearUmAmigo();
+  const mensagem = `Parabéns: ${amigoSorteado}`;
+
+  const li = document.createElement("li");
+  li.textContent = mensagem;
+  listaResultado.appendChild(li);
+
+  // Limpa a lista
+  amigos = [];
+  atualizarListaAmigos();
+}
+
+function sortearUmAmigo() {
+  const indiceSorteado = Math.floor(Math.random() * amigos.length);
+  return amigos[indiceSorteado];
+}
